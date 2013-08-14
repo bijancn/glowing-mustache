9347f99c4bc5fe28b97795588930847fa004f89e
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2013-08-14 18:00:40 +0200

    initial gcal export to latex

---
+++ gcal_export.py
@@ -0,0 +1,19 @@
+from calendar_parser import CalendarParser
+
+ics_feed = "https://www.google.com/calendar/ical/4rc1vqnpc2r7nqpaboi037vf1k%40group.calendar.google.com/public/basic.ics"
+
+cal = CalendarParser(ics_url=ics_feed)
+time_hl = 'textbf'
+location_hl = 'textit'
+
+def tex_event(event):
+  strg = "  if (equals=" + str(event.start_time.date()) + ") [observance="
+  strg += " \\" + time_hl + "{" + str(event.start_time.time())[:5] + "} "
+  strg +=  event.name
+  if 'location' in event.keys():
+    strg += " \\" + location_hl + "{" + event.location + "}"
+  print strg + "]"
+
+for event in cal.parse_calendar():
+  tex_event(event)
+  print event.all_day
