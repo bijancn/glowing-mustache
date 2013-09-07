e9541db8cdef9a4e976b8659d16dbc3de0d71901
Bijan Chokoufe Nejad <bijan@chokoufe.com>
2013-09-07 11:20:12 +0200

    Added unicode support. Corresponding Python-GoogleCalendarParser fork is neededvim README.md  Escaped some characters for latex and set cutoff.

--- README.md
+++ README.md
@@ -5,16 +5,7 @@ LaTeX Calender
 
 Upcoming Milestones:
 --------------------
-- GitHub Repo aufsetzen (und mich einladen ;))
-- Python-GoogleCalendarParser ausprobieren. Event title und termin
-ausgeben lassen.
-- TeX Vorlage durcharbeiten. Befehle verstehen und kommentieren.
-Vereinfachen. Mir ist zB noch nicht klar, wie man jeden Monat auf ein
-DIN A4 Blatt macht. Wenn man die Range bei dem TikZ erhoeht, haengt er
-das einfach an das gleiche TikZ Bild dran.
-- \include von ein paar Test Terminen, die man sich in ein extra File
-gcal.tex schreibt.
-- Erzeugung eines solchen Test file von Python aus.
+- TeX Vorlage weiter durcharbeiten. Befehle verstehen und kommentieren.
 
 Die Grundlage ist Googles API
 https://developers.google.com/google-apps/calendar/v2/developers_guide_protocol?hl=de&csw=1#RetrievingEvents
@@ -35,16 +26,3 @@ nicht, waere das
 http://blog.nguyenvq.com/backup-my-google-calendars-via-shell-and-python-scripts-and-scheduling-them-via-cron/
 auch eine Moeglichkeit. Waere vielleicht sowieso nicht schlecht
 Backups vom Google Calendar zu haben.
-
-Angenommen obiges klappt, haben wir jetzt Events als Python Objekte.
-Der naechste Schritt ist, daraus Strings zu machen, die fuer TeX
-lesbar sind. Im Anhang habe ich eine norwegische Vorlage fuer Tikz
-Calendar, an der ich bischen rumgespielt habe. Da muss man sich auch
-etwas reinarbeiten. Insbesondere sollte sie so allgemein und simpel
-wie moeglich gehalten werden und die ganzen laenderspezifischen
-Sachen, wie Feiertage, sollten aus Google Calendar folgen. Die
-wichtige Syntax habe ich im TeX file markiert. Es scheint sowas wie
-  if (equals=07-29) [observance=Olsok]
-zu sein. Kenne mich aber auch noch nicht so aus. Am besten waere es
-wenn man an die Stelle ein \include einfuegen kann und dann das tex
-file einbindet, das man mit Python generiert. Geht aber auch anders.
--- gcal_export.py
+++ gcal_export.py
@@ -1,22 +1,28 @@
 from calendar_parser import CalendarParser
 import datetime as dt
+# todo: Yearly reoccurring birthdays are not captured :(
 
 ics_feed = "https://www.google.com/calendar/ical/4rc1vqnpc2r7nqpaboi037vf1k%40group.calendar.google.com/public/basic.ics"
-
 cal = CalendarParser(ics_url=ics_feed)
 time_hl = 'textbf'
 location_hl = 'textit'
+print_KW = False
+event_name_cutoff = 30
+f = open('templates/gcal_out.tex', 'w')
 
 # returns the event as LaTeX formatted string, suitable for our template
 def tex_event(event, eventtime):
-  strg = "  if (equals=" + str(eventtime.date()) + ") [observance="
+  strg = "  if (equals=" + str(eventtime.date()) + ") [observance={"
   looks_all_day = event.end_time.time() == event.start_time.time() 
   if not (event.all_day or looks_all_day):
-    strg += " \\" + time_hl + "{" + str(eventtime.time())[:5] + "} "
-  strg +=  event.name
+    strg += "\\" + time_hl + "{" + str(eventtime.time())[:5] + "} "
+  if len(event.name) > event_name_cutoff:
+    strg +=  event.name[:event_name_cutoff] + '..'
+  else:
+    strg +=  event.name
   if 'location' in event.keys():
-    strg += " \\" + location_hl + "{" + event.location + "}"
-  strg += "]"
+    strg += ", \\" + location_hl + "{" + event.location + "}"
+  strg += "}]\n"
   return strg
 
 a_day = dt.timedelta(days=1)
@@ -27,7 +33,7 @@ def all_event(event):
   strg = tex_event(event, event.start_time)
   if (event.end_time.date() - event.start_time.date() > a_day):
     for i in range(1, (event.end_time.date() - event.start_time.date()).days):
-      strg += '\n' + tex_event(event, event.start_time + i * a_day)
+      strg += tex_event(event, event.start_time + i * a_day)
   return strg
 
 # todo: play with these options for improved look
@@ -40,24 +46,35 @@ def preamble(startdate, enddate):
   strg += '            day headings={font=\small},\n'
   strg += '            day letter headings]\n'
   strg += '  if (Sunday) [red]\n'
-  strg += '  if(Monday,equals=01-01,equals=02-01,equals=03-01,equals=04-01,\n'
-  strg += '            equals=05-01,equals=06-01,equals=07-01,equals=08-01,\n'
-  strg += '            equals=09-01,equals=10-01,equals=11-01,equals=12-01)\n'
-  strg += '    [week number]'
+  if print_KW:
+    strg += '  if(Monday,equals=01-01,equals=02-01,equals=03-01,equals=04-01,\n'
+    strg += '            equals=05-01,equals=06-01,equals=07-01,equals=08-01,\n'
+    strg += '            equals=09-01,equals=10-01,equals=11-01,equals=12-01)\n'
+    strg += '    [week number]\n'
+  return strg
+
+def tex_escape(strg):
+  strg = strg.replace('&','\&')
+  strg = strg.replace('->','$\\to$')
+  strg = strg.replace('>','$>$')
+  strg = strg.replace('<','$<$')
   return strg
  
 year = 2013
 months = range(1,13)
 for month in months:
   startdate = dt.date(year, month, 1)
+  print "Processing " + str(startdate)
   if month + 1 <= 12:
     enddate = dt.date(year, month + 1, 1) - dt.timedelta(days=1)
   else:
     enddate = dt.date(year + 1, 1, 1) - dt.timedelta(days=1)
-  print preamble(startdate, enddate)
-  # This is highly ineffective. One shouldn't call/test all events for every
-  # month.
+  f.write(preamble(startdate, enddate)) 
+  # todo: This is highly ineffective. One shouldn't call/test all events for
+  # every month. Maybe gather all events first and sort them. I want multiple
+  # calendars as well (e.g. private and holidays)
   for event in cal.parse_calendar():
     if startdate < event.start_time.date() < enddate:
-      print all_event(event)
-  print ';\n\end{tikzpicture}'
+      f.write(tex_escape(all_event(event)).encode('utf-8'))
+  f.write(';\n\end{tikzpicture}\n')
+f.close()
